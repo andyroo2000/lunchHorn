6266c4bbbc053f2ff4e5f8c04cb59ac9535e06f1
Andrew Landry <andrewlandry@gmail.com>
2014-06-15 12:26:32 -0400

    use full paths for audio files

--- sounds.php
+++ sounds.php
@@ -1,15 +1,15 @@
 <?php 
-$meeting_bells = array('SHORT BELL' => '~/www/audio/meeting-bell/short-bell.wav',
-		'MEDIUM BELL' => '~/www/audio/meeting-bell/medium-bell.wav',
-		'LONG BELL' => '~/www/audio/meeting-bell/long-bell.wav'
+$meeting_bells = array('SHORT BELL' => '/home/andrew/www/audio/meeting-bell/short-bell.wav',
+		'MEDIUM BELL' => '/home/andrew/www/audio/meeting-bell/medium-bell.wav',
+		'LONG BELL' => '/home/andrew/www/audio/meeting-bell/long-bell.wav'
 	);
 
-$coffee_triangles = array('SHORT TRIANGLE' => '~/www/audio/coffee-triangle/short-triangle.wav',
-		'MEDIUM TRIANGLE' => '~/www/audio/coffee-triangle/medium-triangle.wav',
-		'LONG TRIANGLE' => '~/www/audio/coffee-triangle/long-triangle.wav'
+$coffee_triangles = array('SHORT TRIANGLE' => '/home/andrew/www/audio/coffee-triangle/short-triangle.wav',
+		'MEDIUM TRIANGLE' => '/home/andrew/www/audio/coffee-triangle/medium-triangle.wav',
+		'LONG TRIANGLE' => '/home/andrew/www/audio/coffee-triangle/long-triangle.wav'
 	);
 
-$lunch_train_whistles = array('SHORT WHISTLE' => '~/www/audio/lunch-train/short-whistle.wav',
-		'MEDIUM WHISTLE' => '~/www/audio/lunch-train/medium-whistle.wav',
-		'LONG WHISTLE' => '~/www/audio/lunch-train/long-whistle.wav'
+$lunch_train_whistles = array('SHORT WHISTLE' => '/home/andrew/www/audio/lunch-train/short-whistle.wav',
+		'MEDIUM WHISTLE' => '/home/andrew/www/audio/lunch-train/medium-whistle.wav',
+		'LONG WHISTLE' => '/home/andrew/www/audio/lunch-train/long-whistle.wav'
 	);
