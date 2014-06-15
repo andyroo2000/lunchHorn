fb643f855a439b2dba603c86e05c39c404f854ee
Andrew Landry <andrewlandry@gmail.com>
2014-06-15 10:51:52 -0400

    set correct path when post request is received

--- play_sound.php
+++ play_sound.php
@@ -2,20 +2,36 @@
 include ('sounds.php'); // include the arrays of sounds
 
 $myArray = json_decode(file_get_contents('php://input'), true);
-$id = $myArray['id'];
 $idParent = $myArray['idParent'];
+$id = $myArray['id'];
+$path_of_bell = '';
 
-foreach ($idParent as $name => $file_location) {
-	if ($name == $id) {
-		$path_of_file_to_play = $file_location;
+if ($idParent == 'meeting-bell') {
+	foreach ($meeting_bells as $name_of_bell => $url_of_bell) {
+		if ($id == $name_of_bell) {
+			$path_of_bell = $url_of_bell;
+		}
 	}
-}
-// $meeting_bell_sounds = array('~/Desktop/test.mp3');
+} 
+
+if ($idParent == 'coffee-triangle') {
+	foreach ($coffee_triangles as $name_of_bell => $url_of_bell) {
+		if ($id == $name_of_bell) {
+			$path_of_bell = $url_of_bell;
+		}
+	}
+} 
+
+if ($idParent == 'lunch-train') {
+	foreach ($lunch_train_whistles as $name_of_bell => $url_of_bell) {
+		if ($id == $name_of_bell) {
+			$path_of_bell = $url_of_bell;
+		}
+	}
+} 
 
 
 
-// $the_error = shell_exec('mpg123 ~/Desktop/test.mp3; echo $?'); 
-// exec ( 'mpg123 ~/Desktop/test.mp3' , $output, $return );
 
 
 // shell_exec('play ~/Desktop/correctamundo.wav');
@@ -24,10 +40,11 @@ foreach ($idParent as $name => $file_location) {
 $myFile = '~/Desktop/andrew.txt';
 // $id = 'test';
 shell_exec("echo $idParent > $myFile");
+shell_exec("echo $id >> $myFile");
+shell_exec("echo $path_of_bell >> $myFile");
 
-// shell_exec('echo $id > ~/Desktop/andrew.txt');
-// shell_exec('print_r($return) > ~/Desktop/andrew.txt');
-// shell_exec('mpg123 $meeting_bell_sounds[rand(lengthOf($meeting_bell_sounds))]'); 
+
+shell_exec('mpg123 $path_of_bell'); 
 
 // shell_exec("sudo /usr/local/bin/mpg123 ~/Desktop/test.mp3" ); 
 
--- sounds.php
+++ sounds.php
@@ -1,15 +1,15 @@
 <?php 
-$meeting_bells = array('SHORT BELL' => '~/Desktop/test.mp3',
-		'MEDIUM BELL' => '~/Desktop/test.mp3',
-		'LONG BELL' => '~/Desktop/test.mp3'
+$meeting_bells = array('SHORT BELL' => '/audio/short-bell.mp3',
+		'MEDIUM BELL' => '~/Desktop/medium-bell.mp3',
+		'LONG BELL' => '~/Desktop/long-bell.mp3'
 	);
 
-$coffee_triangles = array('SHORT TRIANGLE' => '~/Desktop/test.mp3',
-		'MEDIUM TRIANGLE' => '~/Desktop/test.mp3',
-		'LONG TRIANGLE' => '~/Desktop/test.mp3'
+$coffee_triangles = array('SHORT TRIANGLE' => '~/Desktop/short-triangle.mp3',
+		'MEDIUM TRIANGLE' => '~/Desktop/medium-triangle.mp3',
+		'LONG TRIANGLE' => '~/Desktop/long-triangle.mp3'
 	);
 
-$lunch_train_whistles = array('SHORT WHISTLE' => '~/Desktop/test.mp3',
-		'MEDIUM WHISTLE' => '~/Desktop/test.mp3',
-		'LONG WHISTLE' => '~/Desktop/test.mp3'
+$lunch_train_whistles = array('SHORT WHISTLE' => '~/Desktop/short-whistle.mp3',
+		'MEDIUM WHISTLE' => '~/Desktop/medium-whistle.mp3',
+		'LONG WHISTLE' => '~/Desktop/long-whistle.mp3'
 	);
