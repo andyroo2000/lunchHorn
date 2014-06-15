35bddd10eeffebff8ceaa395c10737ee78cc4f6b
Andrew Landry <andrewlandry@gmail.com>
2014-06-15 11:55:21 -0400

    fix paths for audio files in code and restructure in directories

--- sounds.php
+++ sounds.php
@@ -1,15 +1,15 @@
 <?php 
-$meeting_bells = array('SHORT BELL' => '/audio/short-bell.mp3',
-		'MEDIUM BELL' => '~/Desktop/medium-bell.mp3',
-		'LONG BELL' => '~/Desktop/long-bell.mp3'
+$meeting_bells = array('SHORT BELL' => '/audio/meeting-bell/short-bell.wav',
+		'MEDIUM BELL' => '/audio/meeting-bell/medium-bell.wav',
+		'LONG BELL' => '/audio/meeting-bell/long-bell.wav'
 	);
 
-$coffee_triangles = array('SHORT TRIANGLE' => '~/Desktop/short-triangle.mp3',
-		'MEDIUM TRIANGLE' => '~/Desktop/medium-triangle.mp3',
-		'LONG TRIANGLE' => '~/Desktop/long-triangle.mp3'
+$coffee_triangles = array('SHORT TRIANGLE' => '/audio/coffee-triangle/short-triangle.wav',
+		'MEDIUM TRIANGLE' => '/audio/coffee-triangle/medium-triangle.wav',
+		'LONG TRIANGLE' => '/audio/coffee-triangle/long-triangle.wav'
 	);
 
-$lunch_train_whistles = array('SHORT WHISTLE' => '~/Desktop/short-whistle.mp3',
-		'MEDIUM WHISTLE' => '~/Desktop/medium-whistle.mp3',
-		'LONG WHISTLE' => '~/Desktop/long-whistle.mp3'
+$lunch_train_whistles = array('SHORT WHISTLE' => '/audio/lunch-train/short-whistle.wav',
+		'MEDIUM WHISTLE' => '/audio/lunch-train/medium-whistle.wav',
+		'LONG WHISTLE' => '/audio/lunch-train/long-whistle.wav'
 	);
